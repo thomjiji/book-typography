615d446fe45107507360c070ff5ba2bf6efe5531
thomjiji <hxy449999@gmail.com>
2024-02-26 10:47:23 +0800

    Refine docs
    Signed-off-by: thomjiji <hxy449999@gmail.com>

--- punc_conv.py
+++ punc_conv.py
@@ -1,13 +1,14 @@
-"""Convert half-width punctuation to full-width punctuation in an HTML file.
-
-Convert half-width punctuation to full-width punctuation in an HTML file. When English
-characters are preceded and followed by Chinese characters, add an appropriate amount of
-margin before and after them.
+"""Convert half-width punctuation to full-width punctuation in an HTML file. Add margin
+around English characters and numbers when they are preceded and followed by Chinese
+characters.
 
 This script reads HTML files, parses its content, and replaces all half-width
 punctuation characters with their equivalent full-width punctuation characters in the
-text content of HTML elements. The modified HTML content is then written back to the
-original file.
+text content of HTML elements. It finds all English characters and numbers that preceded
+and followed by Chinese characters, adds a html tag to it, then uses css to add margin
+around them.
+
+The modified HTML content is then written back to the original file.
 """
 
 import argparse
