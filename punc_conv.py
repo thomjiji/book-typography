15f5d2be1be4f6dfdf3d10a250798726a1ff2a4f
thomjiji <hxy449999@gmail.com>
2024-03-10 16:30:37 +0800

    New condition for margin_add_left
    Signed-off-by: thomjiji <hxy449999@gmail.com>

--- punc_conv.py
+++ punc_conv.py
@@ -65,7 +65,7 @@ def half_to_full_width(text):
         text = text.replace(half, full)
 
     text = re.sub(
-        r"([\u4e00-\u9fff])([A-Za-z0-9✕%éèàçâêîôûëïüÿœæœÆŒÉÈÀÇÂÊÎÔÛËÏÜŸ]+)(?=[ .,—（），。'“”？])",
+        r"([\u4e00-\u9fff])([A-Za-z0-9✕%éèàçâêîôûëïüÿœæœÆŒÉÈÀÇÂÊÎÔÛËÏÜŸ]+)(?=[ .,—（），。'“”？、])",
         r"\1<span class='margin_add_left'>\2</span>",
         text,
     )
